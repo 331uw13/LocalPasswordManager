1e676d914473cd44636461b43ffefc0f88411311
331uw13 <69628281+331uw13@users.noreply.github.com>
2025-06-25 12:30:10 +0000

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,158 +1,157 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
 
-#include "manager.h"
+#include "lsxd.h"
+#include "config.h"
 
 
 
-#define ACTION_SET_ENT 0
-#define ACTION_GET_ENT 1
+void main_loop(struct lsxd_t* st) {
+   
+    while(st->running) {
+
+        const size_t max_i = st->scroll + st->num_rows;
+        int line_count = 0;
+
+        // TODO: Make configurable top bar
+
+        printf("\e[2K\e[90m%c %s\e[0m\n", (st->scroll > 0) ? '^' : '-', st->cwd);
+        line_count++;
+
+        for(size_t i = st->scroll; i < max_i; i++) {
+            if(i >= st->buf.num_files) {
+                break;
+            }
+            int is_selected = (i == (size_t)st->cursori);
+            print_file_info(st, &st->buf.files[i], is_selected);
+            line_count++;
+        }
+
+        printf("\e[2K%s\n", (max_i == st->buf.num_files) ? st->cfg.bottom_line : "\e[2K");
+        line_count++;
 
 
-void print_usage(char* arg0) {
-    printf(
-            "%s set <entry> <file> : Write new encrypted entry to a file.\n"
-            "%s get <entry> <file> : Read and decrypt existing entry.\n"
-            "\n"
-            "Optional flags:\n"
-            "-c  :  Copy \"get\" action result to clipboard instead of printing it to stdout.\n"
-            "\n"
-            "\"set\" action will create a new file with permissions 600 if it doesnt exist.\n"
-            ,
-            arg0, arg0
-            );
+        if(st->last_line_count > line_count) {
+            // Current line count is smaller.
+            // Clear all lines that are not needed anymore.
+            int rem = st->last_line_count - line_count;
+            for(int i = 0; i < rem; i++) {
+                printf("\e[2K\n");
+            }
+            line_count += rem;
+        }
+
+
+        st->last_line_count = line_count;
+        handle_input(st);
+        mv_cursor_up(line_count);
+    }
 }
 
 
+// Escape sequence start '\e' has to be replaced with
+// 0x1B(escape) because config reader reads it as literal '\e'
+void replace_esc_byte(char* array, size_t size) {
+
+    for(size_t i = 0; i < size; i++) {
+        if(i+2 >= size) {
+            break;
+        }
+
+        if(array[i] == '\\' && array[i+1] == 'e') {
+            memmove(
+                    &array[i],
+                    &array[i+1],
+                    size - i
+                    );
+
+            array[i] = 0x1B;
+            // last byte of array should be already zero.
+        }
+    }
+}
+
+#define CFGBUF_SIZE 256
+int parse_config(struct lsxd_t* st) {
+    int result = 0; 
+    struct config_file_t cfgfile;
+    if(!open_config_file(&cfgfile, "./config")) {
+        fprintf(stderr, "'%s': Config file failed to open. return 1\n",
+                __func__);
+        goto error;
+    }
+
+    char buf[CFGBUF_SIZE];
+
+    if(read_cfgvar(&cfgfile, "regfile_color", buf, CFGBUF_SIZE)) {
+        st->cfg.regfile_color = atoi(buf);
+    }
+    if(read_cfgvar(&cfgfile, "directory_color", buf, CFGBUF_SIZE)) {
+        st->cfg.directory_color = atoi(buf);
+    }
+    if(read_cfgvar(&cfgfile, "symlink_color", buf, CFGBUF_SIZE)) {
+        st->cfg.symlink_color = atoi(buf);
+    }
+    if(read_cfgvar(&cfgfile, "blockdev_color", buf, CFGBUF_SIZE)) {
+        st->cfg.blockdev_color = atoi(buf);
+    }
+    if(read_cfgvar(&cfgfile, "chardev_color", buf, CFGBUF_SIZE)) {
+        st->cfg.chardev_color = atoi(buf);
+    }
+    if(read_cfgvar(&cfgfile, "socket_color", buf, CFGBUF_SIZE)) {
+        st->cfg.socket_color = atoi(buf);
+    }
+    if(read_cfgvar(&cfgfile, "fifo_color", buf, CFGBUF_SIZE)) {
+        st->cfg.fifo_color = atoi(buf);
+    }
+
+    memset(st->cfg.selected_prefix,    0, CFGSTRV_SIZE+1);
+    memset(st->cfg.un_selected_prefix, 0, CFGSTRV_SIZE+1);
+    memset(st->cfg.selected_suffix,    0, CFGSTRV_SIZE+1);
+    memset(st->cfg.un_selected_suffix, 0, CFGSTRV_SIZE+1);
+    memset(st->cfg.bottom_line,        0, CFGSTRV_SIZE+1);
+
+    read_cfgvar(&cfgfile, "selected_prefix", st->cfg.selected_prefix, CFGSTRV_SIZE);
+    read_cfgvar(&cfgfile, "un_selected_prefix", st->cfg.un_selected_prefix, CFGSTRV_SIZE);
+    
+    read_cfgvar(&cfgfile, "selected_suffix", st->cfg.selected_suffix, CFGSTRV_SIZE);
+    read_cfgvar(&cfgfile, "un_selected_suffix", st->cfg.un_selected_suffix, CFGSTRV_SIZE);
+    
+    read_cfgvar(&cfgfile, "bottom_line", st->cfg.bottom_line, CFGSTRV_SIZE);
+   
+
+    replace_esc_byte(st->cfg.selected_prefix, CFGSTRV_SIZE);
+    replace_esc_byte(st->cfg.un_selected_prefix, CFGSTRV_SIZE);
+    replace_esc_byte(st->cfg.selected_suffix, CFGSTRV_SIZE);
+    replace_esc_byte(st->cfg.un_selected_suffix, CFGSTRV_SIZE);
+    replace_esc_byte(st->cfg.bottom_line, CFGSTRV_SIZE);
+
+    close_config_file(&cfgfile);
+
+    result = 1;
+error:
+    return result;
+}
 
 int main(int argc, char** argv) {
+    struct lsxd_t st;
 
-    if(argc != 4) {
-        print_usage(argv[0]);
+
+    if(!parse_config(&st)) {
         return 1;
     }
 
-    char* action_str = argv[1];
-    char* entry_name = argv[2];
-    char* filename = argv[3];
-
-    int action = -1;
-
-    const size_t entry_name_size = strlen(entry_name);
-    const size_t action_str_size = strlen(action_str);
-
-    if(action_str_size != 3) {
-        fprintf(stderr, "\033[31mInvalid action \"%s\"\033[0m\n\n", action_str);
-        print_usage(argv[0]);
+    if(!init_lsxd(&st, DEFAULT_MAX_ROWS)) {
+        fprintf(stderr, "Failed to initialize.\n");
         return 1;
     }
 
-    if(strcmp(action_str, "get") == 0) {
-        action = ACTION_GET_ENT;
-    }
-    else
-    if(strcmp(action_str, "set") == 0) {
-        action = ACTION_SET_ENT;
-    }
-    else {
-        fprintf(stderr, "\033[31mInvalid action \"%s\"\033[0m\n\n", action_str);
-        print_usage(argv[0]);
-        return 1;
-    }
-
-
-    int file_exists = (access(filename, F_OK) == 0);
-
-    if((action == ACTION_GET_ENT) && !file_exists) {
-        fprintf(stderr, "\033[31mAction 'get' must have a valid file as input.\n"
-                        "File \"%s\" not found.\033[0m\n\n", filename);
-        print_usage(argv[0]);
-        return 1;
-    }
-    else
-    if((action == ACTION_SET_ENT) && !file_exists) {
-        // Ask to create a new file.
-
-        int valid_input = 0;
-
-        printf("\"%s\" Doesnt exist. Create new file? [y/n]: ", filename);
-        fflush(0);
-        char read_buf[16+1] = { 0 };
-        int read_size = read(1, read_buf, 16);
-
-        int create_new = 0;
-
-        if(read_size > 1) {
-            if((read_buf[0] == 'y') || (read_buf[0] == 'Y')) {
-                create_new = 1;
-            }
-        }
-
-        if(!create_new) {
-            printf("File not created.\n");
-            return 0;
-        }
-
-        // Only user can read and write to the file.
-        mode_t modes = S_IRUSR | S_IWUSR;
-        creat(filename, modes);
-
-    }
-
-
-    // Confirm that it is a regular file.
-    struct stat sb;
-    if(stat(filename, &sb) != 0) {
-        fprintf(stderr, "Could not get information about \"%s\"\n"
-                "%s\n",
-                filename, strerror(errno));
-        return 1;
-    }
-
-    if((sb.st_mode & S_IFMT) != S_IFREG) {
-        fprintf(stderr, "\"%s\" Is not a regular file.\n",
-                filename);
-        return 1;
-    }
-
-    // Confirm file permissions are okay.
-
-    if((sb.st_mode & S_IXUSR) /* User execute permission */
-    || (sb.st_mode & S_IRGRP) /* Group read permission */
-    || (sb.st_mode & S_IWGRP) /* Group write permission */
-    || (sb.st_mode & S_IXGRP) /* Group execute permission */
-    || (sb.st_mode & S_IROTH) /* Others read permission */
-    || (sb.st_mode & S_IWOTH) /* Others write permission */
-    || (sb.st_mode & S_IXOTH) /* Others execute permission */
-    ){
-        fprintf(stderr, "\033[31m\"%s\" Has invalid permissions."
-                        "\nOnly user write and read should be allowed (600)\033[0m\n\n",
-                        filename);
-
-        print_usage(argv[0]);
-
-        return 1;
-    }
-
-
-
-    if(!init_manager()) {
-        return 1;
-    }
-
-    if(action == ACTION_SET_ENT) {
-        manager_write_entry(entry_name, entry_name_size, filename);
-    }
-    else
-    if(action == ACTION_GET_ENT) {
-        manager_read_entry(entry_name, entry_name_size, filename);
-    }
-
+    main_loop(&st);
+    quit_lsxd(&st);
+    mv_cursor_down(st.last_line_count);
     return 0;
 }
+
 
 
